3d1d791ffa41080adfd5247d81492d6693507e67
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-16 14:38:59 -0500

    * imported @PreAuthorize bean so users have to be logged in to use methods * added @Autowired bean to inject all the necessary DAOs to the ShoppingCartController class * added @GetMapping to the getCart()

--- capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,7 +1,10 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,13 +20,24 @@ import java.security.Principal;
 @RestController
 @RequestMapping("/cart")
 @CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class ShoppingCartController {
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
 
+    @Autowired
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao,
+                                  UserDao userDao,
+                                  ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
+
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping()
     public ShoppingCart getCart(Principal principal) {
         try {
             // get the currently logged in username
