d45c72a973477a3dd51b56b6192a7df705615026
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-16 14:30:30 -0500

    * updated formatting for classes and method * added @RestController bean to method * added @RequestMapping bean * added @CrossOrigin bean so class can talk to other classes

--- capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,9 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -10,22 +13,19 @@ import org.yearup.models.User;
 
 import java.security.Principal;
 
-// convert this class to a REST controller
 // only logged in users should have access to these actions
-public class ShoppingCartController
-{
+@RestController
+@RequestMapping("/cart")
+@CrossOrigin
+public class ShoppingCartController {
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
 
-
-
     // each method in this controller requires a Principal object as a parameter
-    public ShoppingCart getCart(Principal principal)
-    {
-        try
-        {
+    public ShoppingCart getCart(Principal principal) {
+        try {
             // get the currently logged in username
             String userName = principal.getName();
             // find database user by userId
@@ -34,9 +34,7 @@ public class ShoppingCartController
 
             // use the shoppingcartDao to get all items in the cart and return the cart
             return null;
-        }
-        catch(Exception e)
-        {
+        } catch(Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
