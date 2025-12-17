b3d1070a0c4f466f6f15a2a5a869fd0e70a29618
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-17 09:38:28 -0500

    * added dataSource to Connections

--- capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlUserDao.java
+++ capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlUserDao.java
@@ -27,7 +27,7 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao
         String sql = "INSERT INTO users (username, hashed_password, role) VALUES (?, ?, ?)";
         String hashedPassword = new BCryptPasswordEncoder().encode(newUser.getPassword());
 
-        try (Connection connection = getConnection())
+        try (Connection connection = dataSource.getConnection())
         {
             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, newUser.getUsername());
@@ -54,7 +54,7 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao
         List<User> users = new ArrayList<>();
 
         String sql = "SELECT * FROM users";
-        try (Connection connection = getConnection())
+        try (Connection connection = dataSource.getConnection())
         {
             PreparedStatement statement = connection.prepareStatement(sql);
 
@@ -78,7 +78,7 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao
     public User getUserById(int id)
     {
         String sql = "SELECT * FROM users WHERE user_id = ?";
-        try (Connection connection = getConnection())
+        try (Connection connection = dataSource.getConnection())
         {
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, id);
@@ -105,7 +105,7 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao
                 " FROM users " +
                 " WHERE username = ?";
 
-        try (Connection connection = getConnection())
+        try (Connection connection = dataSource.getConnection())
         {
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, username);
