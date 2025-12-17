2c7feaa9d878f6d54e772dabdfdee3026b21b31d
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-17 15:52:17 -0500

    * added constructors

--- capstone-api-starter/src/main/java/org/yearup/models/ShoppingCart.java
+++ capstone-api-starter/src/main/java/org/yearup/models/ShoppingCart.java
@@ -8,6 +8,13 @@ public class ShoppingCart
 {
     private Map<Integer, ShoppingCartItem> items = new HashMap<>();
 
+    public ShoppingCart() {
+    }
+
+    public ShoppingCart(Map<Integer, ShoppingCartItem> items) {
+        this.items = items;
+    }
+
     public Map<Integer, ShoppingCartItem> getItems()
     {
         return items;
