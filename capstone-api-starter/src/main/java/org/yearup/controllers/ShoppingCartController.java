30028634db06c494e52870f6bdd94f2e312c73c5
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-17 15:51:51 -0500

    * added a commented out addProductToCart() that will be used later * added a sout statement to help troubleshoot the getCart()

--- capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -17,7 +14,6 @@ import org.yearup.models.User;
 
 import java.security.Principal;
 
-// only logged in users should have access to these actions
 @RestController
 @RequestMapping("/cart")
 @CrossOrigin
@@ -41,13 +37,13 @@ public class ShoppingCartController {
     public ShoppingCart getCart(Principal principal) {
         ShoppingCart shoppingCart;
         try {
-            // get the currently logged in username
             String userName = principal.getName();
-            // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
             shoppingCart = shoppingCartDao.getByUserId(userId);
+
+            System.out.println("shopping cart in Controller: " + shoppingCart);
 
             return shoppingCart;
         } catch(Exception e) {
@@ -57,6 +53,24 @@ public class ShoppingCartController {
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+//    @PostMapping("/cart/products/{productId}")
+//    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+//    public ShoppingCart addProductToCart(Principal principal, int productId) {
+//        ShoppingCart shoppingCart;
+//        try {
+//            String userName = principal.getName();
+//            User user = userDao.getByUserName(userName);
+//            int userId = user.getId();
+//
+//            shoppingCart = shoppingCartDao.getByUserId(userId);
+//            if (shoppingCart == null) {
+//                return null;
+//            }
+//            return shoppingCart;
+//        } catch(Exception e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+//        }
+//    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
