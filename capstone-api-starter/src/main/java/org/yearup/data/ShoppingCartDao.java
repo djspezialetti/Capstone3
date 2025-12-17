b1b9519195f164d7d52fd772ad471abd6ef2b055
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-17 15:51:05 -0500

    * commented out next methods while working on getByUserId()

--- capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,10 +1,12 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
-    //ShoppingCart
+//    Product addProductToCart(int productId);
+//    ShoppingCart removeProduct(int productId);
 
     // need to get the cart
     // need to add stuff to the cart
