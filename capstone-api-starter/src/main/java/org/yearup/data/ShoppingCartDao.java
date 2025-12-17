e43cf21d19ad11ddb4fa59c9929a7e5e78b288ea
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-17 11:53:36 -0500

    * added notes on what methods I need to add to the interface later

--- capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -4,5 +4,10 @@ import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    //ShoppingCart
+
+    // need to get the cart
+    // need to add stuff to the cart
+    // need to delete stuff from the cart
+    // optional: update stuff in the cart with @PutMapping
 }
