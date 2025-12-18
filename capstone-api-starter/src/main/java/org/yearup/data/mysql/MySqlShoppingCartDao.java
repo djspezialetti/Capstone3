c52e1fa7937174b00534935877f2aed27cb41791
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-18 10:35:28 -0500

    * got the getByUserId() and mapRow() to work * error in that nothing is being displayed in the cart when viewed on the website * working on addProductToCart()

--- capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -9,7 +9,9 @@ import org.yearup.models.ShoppingCartItem;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -20,6 +22,9 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
 
     @Override
     public ShoppingCart getByUserId(int userId) {
+        ShoppingCart theCart = new ShoppingCart();
+        List<ShoppingCartItem> shoppingCart = new ArrayList<>();
+
         String  query = "SELECT user_id, product_id, quantity " +
                 "FROM shopping_cart " +
                 "WHERE user_id = ?;";
@@ -27,51 +32,54 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, userId);
             System.out.println("userid in query: " + userId);
-//            System.out.println("productid in query: ");
-//            System.out.println("userid in query: " + userId);
 
             try (ResultSet results = statement.executeQuery()) {
-                if (results.next()) {
-                    System.out.println("Results in query " + results);
-                    return mapRow(results);
+                while (results.next()) {
+                    ShoppingCartItem cartItems = mapRow(results);
+                    shoppingCart.add(cartItems);
+                    System.out.println("Shopping Cart in query" + shoppingCart.toString());
                 }
             }
         } catch (SQLException e) {
             throw new RuntimeException("Error getting shopping cart by User ID", e);
         }
+        return theCart;
+    }
+
+    @Override
+    public ShoppingCart addProductToCart(int productId) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        List<ShoppingCartItem> cartItem = new ArrayList<>();
+
+        String  query = "INSERT INTO shopping_cart (user_id, product_id, quantity) " +
+                "VALUES (?, ?, ?);";
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, product.getProductId());
+            statement.setInt(2, product.getProductId());
+            statement.setString(3, product.getDescription());
+
+            statement.executeUpdate();
+
+            try (ResultSet keys = statement.getGeneratedKeys()) {
+                if (keys.next()) {
+                    int user_id = keys.getInt(1);
+                    return getByUserId(user_id);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating category...", e);
+        }
         return null;
     }
 
-//    @Override
-//    public Product addProductToCart(Product product) {
-//        String  query = "INSERT INTO shopping_cart (user_id, product_id, quantity) " +
-//                "VALUES (?, ?, ?);";
-//        try (Connection connection = getConnection();
-//             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
-//            statement.setInt(1, product.getProductId());
-//            statement.setInt(2, product.getProductId());
-//            statement.setInt(3, product.);
-//
-//            statement.executeUpdate();
-//
-//            try (ResultSet keys = statement.getGeneratedKeys()) {
-//                if (keys.next()) {
-//                    int user_id = keys.getInt(1);
-//                    return getByUserId(user_id);
-//                }
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Error creating category...", e);
-//        }
-//        return null;
-//    }
 //
 //    @Override
 //    public ShoppingCart removeProduct(int productId) {
 //        return null;
 //    }
 
-    private ShoppingCart mapRow(ResultSet row) throws SQLException {
+    private ShoppingCartItem mapRow(ResultSet row) throws SQLException {
         int userId = row.getInt("user_id");
         int productId = row.getInt("product_id");
         int quantity = row.getInt("quantity");
@@ -79,24 +87,39 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
         System.out.println("productId in mapRow: " + productId);
         System.out.println("quantity in mapRow: " + quantity);
 
-        Product product;
-        ProductDao productDao = null;
+        Product product = new Product();
+        product.setProductId(productId);
+        System.out.println("product: " + product);
 
-        product = productDao.getById(productId);
-        System.out.println("product by ID after the dao: " + product);
-
-        ShoppingCartItem cartItem = new ShoppingCartItem();
-        cartItem.setProduct(product);
-        System.out.println("shopping cart after product is added: " + cartItem);
-
-        Map<Integer, ShoppingCartItem> shoppingCartItem = new HashMap<>();
-        shoppingCartItem.put(userId, cartItem);
-
-        ShoppingCart shoppingCart = new ShoppingCart() {
+        ShoppingCartItem shoppingCartItem = new ShoppingCartItem()
+        {
             {
-                setItems(shoppingCartItem);
+                setProduct(product);
+                setQuantity(quantity);
             }
         };
-        return shoppingCart;
+        System.out.println("shopping cart item(s): " + shoppingCartItem);
+
+        return shoppingCartItem;
+
+//        Product product;
+//        ProductDao productDao = null;
+//
+//        product = productDao.getById(productId);
+//        System.out.println("product by ID after the dao: " + product);
+//
+//        ShoppingCartItem cartItem = new ShoppingCartItem();
+//        cartItem.setProduct(product);
+//        System.out.println("shopping cart after product is added: " + cartItem);
+//
+//        Map<Integer, ShoppingCartItem> shoppingCartItem = new HashMap<>();
+//        shoppingCartItem.put(userId, cartItem);
+//
+//        ShoppingCart shoppingCart = new ShoppingCart() {
+//            {
+//                setItems(shoppingCartItem);
+//            }
+//        };
+//        return null;
     }
 }
